09baf6d8df26f390be51675da41d2598a916a324
Luis Miguel Montoya Henao <luism_999@hotmail.com>
2023-02-12 22:59:24 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://luism31.github.io/',
+  site: 'https://LuisM31.github.io/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
